01ceba3a8ccc29d86d48178e6a17fb3a61686dfa
JulioRobot <juliorobot16@gmail.com>
2025-06-05 16:58:50 +0700

    try make a form

---
+++ src/routes/manage/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';s
+	import { z } from 'zod';
+	export const fromSchema = z.object({
+		username: z.string().min(2).max(50)
+	});
+
+	export type FormSchema = typeof fromSchema;
+</script>
